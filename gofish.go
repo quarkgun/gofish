3423079fc6a27b07bb8addd5bfd818edebfefa97
Marcus Eaton <quarkgun@gmail.com>
2018-06-21 01:57:35 +1000

    playing

--- gofish.go
+++ gofish.go
@@ -7,11 +7,11 @@ import (
 	"strings"
 )
 
-var cardSuites = [4]string{"H", "S", "D", "C"}
-var cardValues = [13]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"}
+var SUITES = [4]string{"Hearts", "Spades", "Diamonds", "Clubs"}
+var VALUES = [13]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"}
 
 var playerHand, cpuHand Hand
-var playerScore, cpuScore int
+var playerScore, cpuScore int = 0,0
 var deck Deck
 
 func main() {
@@ -19,10 +19,10 @@ func main() {
 	fmt.Println("Go Fish!")
 
 	deck = *NewDeck()
-	playerHand = *new(Hand)
-	cpuHand = *new(Hand)
+	playerHand = *NewHand()
+	cpuHand = *NewHand()
 
-	db("Dealing...")
+	fmt.Println("Dealing...")
 	deal()
 
 	playerTurn := true
@@ -30,7 +30,10 @@ func main() {
 	var input string
 
 	for !quit {
-		db("Your Hand", playerHand)
+		fmt.Printf("Scores: Player[%v] CPU[%v]\n", playerScore, cpuScore)
+		fmt.Printf("Your Hand: %v\n", playerHand.size())
+		fmt.Println(playerHand)
+		fmt.Println(cpuHand)
 		if playerTurn {
 			fmt.Print("Choose a card: ")
 			fmt.Scanln(&input)
@@ -39,31 +42,40 @@ func main() {
 				quit = true
 				break
 			} else {
-				db("Do you have a %v?", input)
+				fmt.Printf("Do you have a %v?", input)
 				time.Sleep(time.Second * 1)
 				if cpuHand.hasCard(input) {
-					db("Yep")
+					fmt.Println("Yep")
+					playerHand.removeAllCardsWithValue(input)
+					cpuHand.removeAllCardsWithValue(input)
+					playerScore++
 
 				} else {
-					db("Go Fish!")
+					fmt.Println("Go Fish!")
 					playerTurn = false
+					playerHand.add(deck.take())
 				}
 			}
 
-
 		} else {
-			db("CPU turn...")
+			fmt.Println("CPU turn...")
 			val := cpuHand.randomCard().value
-			db("Do you have a %v?", val)
+			fmt.Printf("Do you have a %v?", val)
 			time.Sleep(time.Second * 1)
 
+			if playerHand.hasCard(val) {
+				fmt.Println("Yep")
+				playerHand.removeAllCardsWithValue(input)
+				cpuHand.removeAllCardsWithValue(input)
+				cpuScore++
+			} else {
+				fmt.Println("Go Fish!")
+				playerTurn = true
+				cpuHand.add(deck.take())
+			}
 		}
 	}
 
-}
-
-func db(f string, s ...interface{}) {
-	fmt.Printf(f, s)
 }
 
 func deal() {
@@ -76,8 +88,8 @@ func deal() {
 func (this *Deck) initDeck() {
 	this.cards = make(map[string][]string)
 
-	for _, s := range cardSuites {
-		for _, val := range cardValues {
+	for _, s := range SUITES {
+		for _, val := range VALUES {
 			this.cards[val] = append(this.cards[val], s)
 		}
 	}
@@ -92,6 +104,12 @@ type Deck struct {
 	cards map[string][]string
 }
 
+func NewHand() *Hand {
+	hand := new(Hand)
+	hand.cards = make(map[string][]string)
+	return hand
+}
+
 func NewDeck() *Deck {
 	deck := new(Deck)
 	deck.initDeck()
@@ -99,40 +117,64 @@ func NewDeck() *Deck {
 }
 
 func (this *Deck) take() Card {
-	defer func() {
-		this.cards = this.cards[1:]
-	}()
-	return this.cards[0]
+	card := this.randomCard()
+	this.removeCard(card)
+	fmt.Println("Taking card", card)
+	return card
 }
 
 func (this *Deck) add(card Card) {
-	this.cards = append(this.cards, card)
-}
-
-func (this *Deck) peek() Card {
-	return this.cards[0]
+	this.cards[card.value] = append(this.cards[card.value], card.suite)
 }
 
 func (this *Deck) size() int {
-	count := 0
-	for _, num := range this.cards {
-		count += len(num)
-	}
-	return count
+	return len(this.cards)
 }
 
 func (this *Deck) randomCard() Card {
 //	return this.cards[rand.Intn(len(this.cards))]
+	value := randMapKey(this.cards)
+	suites := this.cards[value]
 
-	
+	if len(suites) > 0 {
+		suite := randListVal(suites)
+		return Card{value:value, suite:suite}
+	}
+	panic("Tried to get an invalid card")
+}
 
+func randListVal(list []string) string {
+	return list[rand.Intn(len(list))]
+}
 
+func randMapKey(m map[string][]string) string {
+	i := rand.Intn(len(m))
+	for k := range m {
+		if i == 0 {
+			return k
+		}
+		i--
+	}
+	panic("never")
 }
 
 func (this *Deck) removeAllCardsWithValue(value string) {
-	for _, c := range this.cards {
+	delete(this.cards, value)
+}
 
+func (this *Deck) removeCard(card Card) {
+	values := this.cards[card.value]
+	for i, s := range values {
+		if s == card.suite {
+			deleteElementAt(values, i)
+		}
 	}
+}
+
+func deleteElementAt(slice []string, index int) {
+	slice[index] = slice[len(slice)-1] // Copy lslicest element to index i.
+	slice[len(slice)-1] = ""   // Erslicese lslicest element (write zero vslicelue).
+	slice = slice[:len(slice)-1]   // Truncslicete slice.
 }
 
 type Hand struct {
@@ -140,14 +182,23 @@ type Hand struct {
 }
 
 func (this *Hand) hasCard(val string) bool {
-	for _, c := range this.cards {
-		if c.value == val {
-			return true
-		}
-	}
-	return false
+	values := this.cards[val]
+	return len(values) > 0
 }
 
 func (this *Hand) addCard(card Card) {
 	this.add(card)
+}
+
+func IndexOf(vs []string, t string) int {
+	for i, v := range vs {
+		if v == t {
+			return i
+		}
+	}
+	return -1
+}
+
+func Contains(vs []string, t string) bool {
+	return IndexOf(vs, t) >= 0
 }
